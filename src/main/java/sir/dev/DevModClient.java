7f9cda4505e2988c18c08b69dc32292058bb3e04
Sir Devenilla <90048149+DEVENILLA@users.noreply.github.com>
2023-02-24 09:24:33 +0200

    just added mod client

---
+++ src/main/java/sir/dev/DevModClient.java
@@ -0,0 +1,10 @@
+package sir.dev;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DevModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
